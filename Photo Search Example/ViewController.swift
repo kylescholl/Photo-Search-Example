33f31f66461945d4dc71196c343b56af81da9b58
mccoyLBI <kyle.b.scholl@gmail.com>
2016-01-15 10:47:12 -0500

    changes

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -9,71 +9,69 @@
 import UIKit
 import AFNetworking
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
 	@IBOutlet var scrollView: UIScrollView!
 	
+	
+	//MARK: ViewController lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+		searchInstagramByHashtag("dogs")
+	}
+	
+	//MARK: Utility methods
+	func searchInstagramByHashtag(searchString:String) {
 		let manager = AFHTTPSessionManager()
 		
-		// "https://api.instagram.com/v1/tags/kscholl16/media/recent?client_id=c4fc61c4704949baab8825cf178e13fe"
-		// 787b724208d04670bb11a4e5fe22aef2
-		// need an access_code, not client_id
+		//var myID : String        = "787b724208d04670bb11a4e5fe22aef2"
+		//var mrDayID : String     = "c4fc61c4704949baab8825cf178e13fe"
+		let thinkfulID : String  = "c4fc61c4704949baab8825cf178e13fe"
 		
-		//	Mine
-		//	https://api.instagram.com/v1/tags/latergram/media/recent?client_id=787b724208d04670bb11a4e5fe22aef2
-		
-		// Mr. Day's
-		//
-		
-		// Thinkful's
-		// https://api.instagram.com/v1/tags/clararockmore/media/recent?client_id=c4fc61c4704949baab8825cf178e13fe
-		
-		let myID : String = "787b724208d04670bb11a4e5fe22aef2"
-		let mrDayID : String =    "c4fc61c4704949baab8825cf178e13fe"
-		let thinkfulID : String = "c4fc61c4704949baab8825cf178e13fe"
-		
-		var searchString : String = "kscholl16"
-		searchString = "clararockmore"
-		let url = "https://api.instagram.com/v1/tags/\(searchString)/media/recent?client_id=\(thinkfulID)"
-		
-		manager.GET(url,
+		manager.GET("https://api.instagram.com/v1/tags/\(searchString)/media/recent?client_id=\(thinkfulID)",
 			parameters: nil,
 			progress: nil,
 			success: { (operation: NSURLSessionDataTask,responseObject: AnyObject?) in
-				print("Success")
 				if let responseObject = responseObject {
-					print("Response: " + responseObject.description)
-				}
-				
-				if let dataArray = responseObject!["data"] as? [AnyObject] {
-					var urlArray:[String] = []
-					for dataObject in dataArray {
-						if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
-							urlArray.append(imageURLString)
+					if let dataArray = responseObject["data"] as? [AnyObject] {
+						var urlArray:[String] = []
+						for dataObject in dataArray {
+							if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+								urlArray.append(imageURLString)
+							}
 						}
-					}
-					
-					self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
-					
-					for var i = 0; i < urlArray.count; i++ {
-						let imageData = NSData(contentsOfURL: NSURL(string: urlArray[i])!)
-						if let imageDataUnwrapped = imageData {
-							let imageView = UIImageView(image: UIImage(data: imageDataUnwrapped))
-							imageView.frame = CGRectMake(0, 320 * CGFloat(i), 320, 320)
-							self.scrollView.addSubview(imageView)
+						//display urlArray in ScrollView
+						self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+						
+						for var i = 0; i < urlArray.count; i++ {
+							let imageView = UIImageView(frame: CGRectMake(0, 320*CGFloat(i), 320, 320))
+							if let url = NSURL(string: urlArray[i]) {
+								imageView.setImageWithURL( url)
+								self.scrollView.addSubview(imageView)
+							}
 						}
+						
 					}
 				}
 			},
-			
 			failure: { (operation: NSURLSessionDataTask?,error: NSError) in
-				print("Failure")
 				print("Error: " + error.localizedDescription)
 		})
+		
 	}
+	//MARK: UISearchBarDelegate protocol methods
+	func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+		for subview in self.scrollView.subviews {
+			subview.removeFromSuperview()
+		}
+		searchBar.resignFirstResponder()
+		if let searchText = searchBar.text {
+			searchInstagramByHashtag(searchText)
+		}
+		
+	}
+}
+
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
