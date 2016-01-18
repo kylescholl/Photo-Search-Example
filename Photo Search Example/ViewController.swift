a26f7a2678311e240384f469f0fda878f12439bd
mccoyLBI <kyle.b.scholl@gmail.com>
2016-01-18 11:59:20 -0500

    stuff

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -9,19 +9,19 @@
 import UIKit
 import AFNetworking
 
-class ViewController: UIViewController, UISearchBarDelegate {
+class ViewController: UIViewController {
 
 	@IBOutlet var scrollView: UIScrollView!
 	
 	
-	//MARK: ViewController lifecycle
+	//MARK: - ViewController lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		searchInstagramByHashtag("dogs")
 	}
 	
-	//MARK: Utility methods
-	func searchInstagramByHashtag(searchString:String) {
+	//MARK: - Utility methods
+	func searchInstagramByHashtag(searchString: String) {
 		let manager = AFHTTPSessionManager()
 		
 		//var myID : String        = "787b724208d04670bb11a4e5fe22aef2"
@@ -31,20 +31,21 @@ class ViewController: UIViewController, UISearchBarDelegate {
 		manager.GET("https://api.instagram.com/v1/tags/\(searchString)/media/recent?client_id=\(thinkfulID)",
 			parameters: nil,
 			progress: nil,
-			success: { (operation: NSURLSessionDataTask,responseObject: AnyObject?) in
+			success: { (operation: NSURLSessionDataTask, responseObject: AnyObject?) in
 				if let responseObject = responseObject {
 					if let dataArray = responseObject["data"] as? [AnyObject] {
-						var urlArray:[String] = []
+						var urlArray : [String] = []
 						for dataObject in dataArray {
 							if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
 								urlArray.append(imageURLString)
 							}
 						}
 						//display urlArray in ScrollView
-						self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+						let imageWidth = self.view.frame.width
+						self.scrollView.contentSize = CGSizeMake(imageWidth, imageWidth * CGFloat(dataArray.count))
 						
 						for var i = 0; i < urlArray.count; i++ {
-							let imageView = UIImageView(frame: CGRectMake(0, 320*CGFloat(i), 320, 320))
+							let imageView = UIImageView(frame: CGRectMake(0, imageWidth*CGFloat(i), imageWidth, imageWidth))
 							if let url = NSURL(string: urlArray[i]) {
 								imageView.setImageWithURL( url)
 								self.scrollView.addSubview(imageView)
@@ -57,27 +58,24 @@ class ViewController: UIViewController, UISearchBarDelegate {
 			failure: { (operation: NSURLSessionDataTask?,error: NSError) in
 				print("Error: " + error.localizedDescription)
 		})
-		
 	}
-	//MARK: UISearchBarDelegate protocol methods
+	//MARK: - UISearchBarDelegate protocol methods
 	func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+		print("searchBar clicked")
 		for subview in self.scrollView.subviews {
 			subview.removeFromSuperview()
 		}
 		searchBar.resignFirstResponder()
 		if let searchText = searchBar.text {
 			searchInstagramByHashtag(searchText)
+			print("search instagram true")
 		}
-		
 	}
-}
-
-
+	
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
 }
 
